f358c6a30ab4e9767f360747a41d4b1037028364
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-04-28 11:00:00 -0400

    Implements merging new followee's Tweets into timeline

--- app.rb
+++ app.rb
@@ -20,16 +20,29 @@ rabbit.start
 channel = rabbit.create_channel
 
 follower_ids = channel.queue('new_tweet.follower_ids')
+new_follow_timeline_data = channel.queue('new_follow.timeline_data')
 
 # Takes a new Tweet's follower_ids payload and updates its followers' cached Timeline Tweet IDs.
 follower_ids.subscribe(block: false) do |delivery_info, properties, body|
   fanout_to_timelines(JSON.parse(body))
 end
 
+new_follow_timeline_data.subscribe(block: false) do |delivery_info, properties, body|
+  merge_into_timeline(JSON.parse(body))
+end
+
 # Adds a new Tweet's ID to each follower's Timeline Tweet IDs in Redis.
 def fanout_to_timelines(body)
   tweet_id = body['tweet_id'].to_i
   body['follower_ids'].each do |follower_id|
-    REDIS.zadd(follower_id, tweet_id, tweet_id) # 1st tweet_id param is set entry's sorting "score."
+    REDIS.zadd(follower_id.to_i, tweet_id, tweet_id) # 1st tweet_id param is set entry's sorting "score."
   end
+end
+
+# Adds new followee's Tweets to follower's imeline Tweet IDs in Redis.
+def merge_into_timeline(body)
+  follower_id = body['follower_id'].to_i
+  body['followee_tweets'].each do |tweet_id|
+    Redis.zadd(follower_id, tweet_id.to_i, tweet_id.to_i) # Using tweet_id as sorting "score" preserves
+  end                                                     # chronological order.
 end
