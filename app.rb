b928df1f9ed675d4af70d1daab2fafedb2d6879c
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-04-28 17:30:22 -0400

    Updates seed handling

--- app.rb
+++ app.rb
@@ -18,14 +18,16 @@ end
 
 rabbit.start
 channel = rabbit.create_channel
+rabbit_exchange = channel.default_exchange
 
 follower_ids = channel.queue('new_tweet.follower_ids')
 new_follow_timeline_data = channel.queue('new_follow.timeline_data')
-seed = channel.queue('timeline.seed')
+new_follow_sorted_tweets = channel.queue('new_follow.sorted_tweets')
+seed = channel.queue('tweet.data.seed')
 
 seed.subscribe(block: false) do |delivery_info, properties, body|
   REDIS.flushall
-  seed_to_timeline(JSON.parse(body))
+  seed_to_timelines(JSON.parse(body))
 end
 
 # Takes a new Tweet's follower_ids payload and updates its followers' cached Timeline Tweet IDs.
@@ -38,10 +40,11 @@ new_follow_timeline_data.subscribe(block: false) do |delivery_info, properties, 
 end
 
 def seed_to_timelines(body)
-  body.each do |tp|
-    owner_id = tp['owner_id'].to_i
-    tweet_id = tp['tweet_id'].to_i
-    REDIS.zadd(owner_id.to_i, tweet_id, tweet_id)
+  body.each do |item|
+    owner_id = item['owner_id'].to_i
+    sorted_tweet_ids = []
+    item['sorted_tweets'].each { |t| sorted_tweet_ids << t.id.to_i }
+    REDIS.zadd(owner_id.to_i, sorted_tweet_ids)
   end
 end
 
@@ -61,4 +64,9 @@ def merge_into_timeline(body)
     tweet_entries << [tweet_id.to_i, tweet_id.to_i] # Tweet_id as sorting "score" preserves chronology
   end
   Redis.zadd(follower_id, tweet_entries) # Bulk add
+  payload = {
+    follower_id: follower_id,
+    sorted_tweet_ids: Redis.zrange(follower_id, 0, -1)
+  }.to_json
+  rabbit_exchange.publish(payload, routing_key: new_follow_sorted_tweets)
 end
