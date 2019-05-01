a37358cdae777c75f0171eb6788da224d8cb0b7c
Ari Carr <acarr@brandeis.edu>
2019-05-01 16:02:07 -0400

    Sharded cache

--- app.rb
+++ app.rb
@@ -10,12 +10,15 @@ set :port, 8082 unless Sinatra::Base.production?
 
 if Sinatra::Base.production?
   configure do
+    REDIS_EVEN = redis_from_uri('REDIS_EVEN_URL')
+    REDIS_ODD = redis_from_uri('REDIS_ODD_URL')
     redis_uri = URI.parse(ENV['REDIS_URL'])
     REDIS = Redis.new(host: redis_uri.host, port: redis_uri.port, password: redis_uri.password)
   end
   rabbit = Bunny.new(ENV['CLOUDAMQP_URL'])
 else
-  REDIS = Redis.new(port: 6386)
+  REDIS_EVEN = Redis.new(port: 6386)
+  REDIS_ODD = Redis.new(port: 6388)
   rabbit = Bunny.new(automatically_recover: false)
 end
 
@@ -32,7 +35,7 @@ seed.subscribe(block: false) do |delivery_info, properties, body|
   seed_to_timelines(JSON.parse(body))
 end
 
-cache_purge.subscribe(block: false) { REDIS.flushall }
+cache_purge.subscribe(block: false) { [REDIS_EVEN, REDIS_ODD].each(&:flushall) }
 
 # Takes a new Tweet's follower_ids payload and updates its followers' cached Timeline Tweet IDs.
 follower_ids.subscribe(block: false) do |delivery_info, properties, body|
@@ -43,20 +46,23 @@ new_follow_timeline_data.subscribe(block: false) do |delivery_info, properties, 
   merge_into_timeline(JSON.parse(body))
 end
 
+def get_shard(owner_id)
+  owner_id.even? ? REDIS_EVEN : REDIS_ODD
+end
+
 def seed_to_timelines(body)
   owner_id = body['owner_id'].to_i
-  unless owner_id == -1
-    sorted_tweet_ids = body['sorted_tweets'].map { |t| t['tweet_id'].to_i }
-    sorted_tweet_ids.each { |i| REDIS.zadd(owner_id.to_i, i, i) }
-    puts "Seeded timeline for user #{owner_id}"
-  end
+  sorted_tweet_ids = body['sorted_tweets'].map { |t| t['tweet_id'].to_i }
+  shard = get_shard(owner_id)
+  sorted_tweet_ids.each { |i| shard.zadd(owner_id.to_i, i, i) }
+  puts "Seeded timeline for user #{owner_id}"
 end
 
 # Adds a new Tweet's ID to each follower's Timeline Tweet IDs in Redis.
 def fanout_to_timelines(body)
   tweet_id = body['tweet_id'].to_i
   body['follower_ids'].each do |follower_id|
-    REDIS.zadd(follower_id.to_i, tweet_id, tweet_id) # 1st tweet_id param is set entry's sorting "score."
+    get_shard(follower_id.to_i).zadd(follower_id.to_i, tweet_id, tweet_id) # 1st tweet_id param is set entry's sorting "score."
   end
 end
 
@@ -64,10 +70,11 @@ end
 def merge_into_timeline(body)
   follower_id = body['follow_params']['follower_id'].to_i
   tweet_entries = body['followee_tweet_ids'].map(&:to_i).map { |tweet_id| [tweet_id, tweet_id] } # Tweet_id as sorting "score" preserves chronology
-  REDIS.zadd(follower_id, tweet_entries) # Bulk add
+  shard = get_shard(follower_id)
+  shard.zadd(follower_id, tweet_entries) # Bulk add
   payload = {
     follower_id: follower_id,
-    sorted_tweet_ids: REDIS.zrange(follower_id, 0, -1)
+    sorted_tweet_ids: shard.zrange(follower_id, 0, -1)
   }.to_json
   RABBIT_EXCHANGE.publish(payload, routing_key: 'new_follow.sorted_tweets')
 end
--- test.rb
+++ test.rb
@@ -14,7 +14,7 @@ def app
 end
 
 def get_timeline(owner_id)
-  REDIS.zrange(owner_id, 0, -1)
+  get_shard(owner_id).zrange(owner_id, 0, -1)
 end
 
 def publish_new_tweet(payload)
@@ -30,7 +30,7 @@ end
 describe 'NanoTwitter Timeline Data' do
   include Rack::Test::Methods
   before do
-    REDIS.flushall
+    [REDIS_EVEN, REDIS_ODD].each(&:flushall)
   end
 
   it "can put a new tweet in a user's timeline" do
@@ -39,7 +39,7 @@ describe 'NanoTwitter Timeline Data' do
       follower_ids: [2]
     }.to_json
     fanout_to_timelines(JSON.parse(payload))
-    REDIS.keys.must_equal ['2']
+    REDIS_EVEN.keys.must_equal ['2']
     get_timeline(2).must_equal ['1']
   end
 
@@ -49,7 +49,7 @@ describe 'NanoTwitter Timeline Data' do
       follower_ids: [2]
     }.to_json
     publish_new_tweet(payload)
-    REDIS.keys.must_equal ['2']
+    REDIS_EVEN.keys.must_equal ['2']
     get_timeline(2).must_equal ['1']
   end
 
@@ -61,7 +61,7 @@ describe 'NanoTwitter Timeline Data' do
       followee_tweet_ids: [2, 1, 3]
     }.to_json
     merge_into_timeline(JSON.parse(payload))
-    REDIS.keys.must_equal ['2']
+    REDIS_EVEN.keys.must_equal ['2']
     get_timeline(2).must_equal %w[1 2 3]
   end
 
@@ -73,7 +73,7 @@ describe 'NanoTwitter Timeline Data' do
       followee_tweet_ids: [2, 1, 3]
     }.to_json
     publish_new_follow(payload)
-    REDIS.keys.must_equal ['2']
+    REDIS_EVEN.keys.must_equal ['2']
     get_timeline(2).must_equal %w[1 2 3]
   end
 
@@ -87,7 +87,8 @@ describe 'NanoTwitter Timeline Data' do
         sorted_tweets: [4, 6, 7, 8].map { |i| { tweet_id: i } }
       }
     ].each { |timeline| seed_to_timelines(JSON.parse(timeline.to_json)) }
-    REDIS.keys.sort.must_equal %w[2 3]
+    REDIS_EVEN.keys.must_equal ['2']
+    REDIS_ODD.keys.must_equal ['3']
     get_timeline(2).must_equal %w[1 2 3]
     get_timeline(3).must_equal %w[4 6 7 8]
   end
@@ -103,7 +104,8 @@ describe 'NanoTwitter Timeline Data' do
       }
     ].each { |timeline| RABBIT_EXCHANGE.publish(timeline.to_json, routing_key: 'timeline.data.seed.timeline_data') }
     sleep 10
-    REDIS.keys.sort.must_equal %w[2 3]
+    REDIS_EVEN.keys.must_equal ['2']
+    REDIS_ODD.keys.must_equal ['3']
     get_timeline(2).must_equal %w[1 2 3]
     get_timeline(3).must_equal %w[4 6 7 8]
   end
